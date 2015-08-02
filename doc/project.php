b07d7c97ea86d9f1727d64e4fc99b8249eb50f23
aidansean <aidan.randleconde@gmail.com>
2015-08-02 20:43:49 +0200

    Added images, reorganised directory structure

--- doc/project.php
+++ doc/project.php
@@ -1,4 +1,6 @@
 <?php
+include_once($_SERVER['FILE_PREFIX']."/project_list/project_object.php") ;
 $github_uri   = "https://github.com/aidansean/project365" ;
 $blogpost_uri = "http://aidansean.com/projects/?tag=project365" ;
+$project = new project_object("project365", "Project 365", "https://github.com/aidansean/project365", "http://aidansean.com/projects/?tag=project365", "365/images/project.jpg", "365/images/project_bw.jpg", "I keep a project 365 page, which means one photo per day.  It's a good way to keep track of the passage of time and as I take my camera everywhere with me it' quite easy.  After using a blog for the photos I soon became frustrated at how long it took, so I decided to make my own wrapper for the images.", "", "") ;
 ?>
--- project.py
+++ project.py
@@ -2,8 +2,9 @@ from project_module import project_object, image_object, link_object, challenge_
 
 p = project_object('project365', 'Project 365')
 p.domain = 'http://www.aidansean.com/'
-p.path = 'project365'
-p.preview_image_ = image_object('http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg', 408, 287)
+p.path = '365'
+p.preview_image    = image_object('%s/images/project.jpg'   %p.path, 150, 250)
+p.preview_image_bw = image_object('%s/images/project_bw.jpg'%p.path, 150, 250)
 p.folder_name = 'aidansean'
 p.github_repo_name = 'project365'
 p.mathjax = False
